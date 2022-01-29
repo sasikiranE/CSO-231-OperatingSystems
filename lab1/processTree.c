b87bce443d3a12c8c864368b1f42d1ab135b2633
sasikiran <sasikiranessamaraju@gmail.com>
2022-01-29 20:59:45 +0530

    initial commit.

---
+++ lab1/processTree.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+int main() {
+    pid_t pid1, pid2, pid3, pid4;
+
+    printf("Parent of all PID : %d\n", getpid());
+
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());
+        pid2 = fork();
+        if (pid2 == 0) {
+            printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());
+        }
+    }
+
+    if (pid1 > 0) {
+        pid3 = fork();
+        if (pid3 == 0) {
+            printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());
+            pid4 = fork();
+            if (pid4 == 0) {
+                printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());
+            }
+        }
+    }
+
+    return 0;
+}
+
+/*
+                A
+              /   \
+            B       C
+           /         \
+         D             E
+
+*/
