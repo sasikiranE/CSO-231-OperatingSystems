f4ed2f79a355f16dc90b228de405f0b6edd5fa06
sasikiran <sasikiranessamaraju@gmail.com>
2022-02-08 17:43:15 +0530

    add missing wait() sys calls.

--- exp1/processTree.c
+++ exp1/processTree.c
@@ -3,6 +3,17 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+/*
+ * 			Creating this kind of Process Tree.
+ *
+ * 							A
+ *
+ *					B				C
+ *
+ *					D				E
+ *
+*/ 					
+
 
 int main() {
     pid_t pid1, pid2, pid3, pid4;
@@ -17,9 +28,11 @@ int main() {
         if (pid2 == 0) {
             printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (D)
         }
+		if(pid2 > 0) wait(NULL);
     }
 
     if (pid1 > 0) {
+		wait(NULL);
         pid3 = fork();
         if (pid3 == 0) {
             printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (C)
@@ -27,17 +40,11 @@ int main() {
             if (pid4 == 0) {
                 printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (E).
             }
+			if(pid4 > 0) wait(NULL);
         }
+		if (pid3 > 0) wait(NULL);
     }
 
     return 0;
 }
 
-/*
-                A
-              /   \
-            B       C
-           /         \
-         D             E
-
-*/
